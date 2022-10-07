e134c6e4c343d2cad3ad0b194326039d43444c39
William Owen <william.owen.bsc@gmail.com>
2022-10-06 17:11:33 -0700

    bug fix - map key

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -41,7 +41,7 @@ export default function Dashboard() {
             <ul className='dash'>
                 {stories.map((el, i) => (
                     <LiStyled
-                        key={i}
+                        key={el.id}
                         data-id={el.id}
                         data-slug={el.meta.slug}
                         status={el.meta.status}
