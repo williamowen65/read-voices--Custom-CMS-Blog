f50cb84c478903c1069ca073cf2458a4284bffc5
William Owen <william.owen.bsc@gmail.com>
2022-10-23 08:53:28 -0700

    img

--- src/firebase-setup.js
+++ src/firebase-setup.js
@@ -39,10 +39,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 
-logEvent(analytics, "notification_received");
 // init services
+// console.log("hi");
 
 export const db = getFirestore();
 export const auth = getAuth();
--- src/pages/story/StoryItem.js
+++ src/pages/story/StoryItem.js
@@ -1,10 +1,20 @@
+import { logEvent } from "firebase/analytics";
 import React, { useRef } from "react";
 import { BiShareAlt } from "react-icons/bi";
 import styled from "styled-components";
 import { ContentPadding } from "../../components/SpecialContainers";
+import { analytics } from "../../firebase-setup";
 
 export default function StoryItem({ story }) {
     const shareBtn = useRef();
+    const handleButtonClick = (text, url) => {
+        window.open(url, "_blank");
+        logEvent(analytics, "select_content", {
+            event_name: "outbound_link",
+            content: story.title,
+            to: text,
+        });
+    };
     return (
         <li>
             <ContentPadding>
@@ -37,33 +47,40 @@ export default function StoryItem({ story }) {
                     />
                 </div>
                 <div className='item'>
-                    <header className='story'>
-                        <h2>{story.title}</h2>
-                        <p>
-                            {story.meta.publishedAt?.slice(
-                                3
-                            )}
-                        </p>
-                    </header>
-                    {/* <span> */}
-                    <p>{story.description}</p>
+                    <img
+                        src='https://via.placeholder.com/150x227'
+                        alt=''
+                        srcset=''
+                    />
+                    <div className='floatBox'>
+                        <header className='story'>
+                            <h2>{story.title}</h2>
+                            <p>
+                                {story.meta.publishedAt?.slice(
+                                    3
+                                )}
+                            </p>
+                        </header>
+                        {/* <span> */}
+                        <p>{story.description}</p>
+                    </div>
                     {/* </span> */}
                 </div>
                 <div className='buttons'>
                     {story.buttons?.length ? (
                         story.buttons.map(
                             (el, i) => (
-                                <button className='storyBtn'>
-                                    <a
-                                        target='_blank'
-                                        rel='noreferrer'
-                                        href={
+                                <button
+                                    className='storyBtn'
+                                    onClick={() =>
+                                        handleButtonClick(
+                                            el.text,
                                             el.link
-                                        }
-                                        key={i}
-                                    >
-                                        {el.text}
-                                    </a>
+                                        )
+                                    }
+                                    key={el.id}
+                                >
+                                    {el.text}
                                 </button>
                             )
                         )
