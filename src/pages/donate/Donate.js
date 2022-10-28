53f57abe55fe13307c7b5182ac1ddbd4983dac88
William Owen <william.owen.bsc@gmail.com>
2022-10-28 10:41:05 -0700

    got all edit btns in place

--- src/pages/donate/Donate.js
+++ src/pages/donate/Donate.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { RiEdit2Fill } from "react-icons/ri";
 import styled from "styled-components";
 import { ContentPadding } from "../../components/SpecialContainers";
 
 export default function Donate() {
     return (
-        <DonateStyled>
-            <ContentPadding>
+        <DonateStyled className='donate'>
+            <ContentPadding classes='contentBox'>
                 <h3>Donate Info</h3>
                 <p>
                     Some instructions on how
                     people can just send money if
                     they want to... Patreon etc...
                 </p>
+                <RiEdit2Fill
+                    className='editWebsiteBtn'
+                    size={20}
+                />
             </ContentPadding>
         </DonateStyled>
     );
