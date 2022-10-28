eed679030e65ce442b2e79ec51b6fda62d269c8e
William Owen <william.owen.bsc@gmail.com>
2022-10-28 11:53:24 -0700

    unsub

--- src/components/util/FetchStories.js
+++ src/components/util/FetchStories.js
@@ -1,5 +1,5 @@
 import { onSnapshot } from "firebase/firestore";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import {
     colRef,
@@ -11,27 +11,41 @@ export default function FetchStories() {
     //get realtime data
     const dispatch = useDispatch();
     try {
-        onSnapshot(orderedCol, (snapshot) => {
-            let stories = [];
-            snapshot.docs.forEach((story) => {
-                stories.push({
-                    ...story.data(),
-                    meta: {
-                        ...story.data().meta,
-                        publishedAt: story
-                            .data()
-                            .meta.publishedAt?.toDate()
-                            ?.toDateString(),
-                        createdAt: story
-                            .data()
-                            .meta.createdAt.toDate()
-                            .toDateString(),
-                    },
-                    id: story.id,
-                });
-            });
-            dispatch(setStories(stories));
-        });
+        useEffect(() => {
+            const unsub = onSnapshot(
+                orderedCol,
+                (snapshot) => {
+                    let stories = [];
+                    snapshot.docs.forEach(
+                        (story) => {
+                            stories.push({
+                                ...story.data(),
+                                meta: {
+                                    ...story.data()
+                                        .meta,
+                                    publishedAt:
+                                        story
+                                            .data()
+                                            .meta.publishedAt?.toDate()
+                                            ?.toDateString(),
+                                    createdAt:
+                                        story
+                                            .data()
+                                            .meta.createdAt.toDate()
+                                            .toDateString(),
+                                },
+                                id: story.id,
+                            });
+                        }
+                    );
+                    dispatch(setStories(stories));
+                }
+            );
+
+            return () => {
+                unsub();
+            };
+        }, []);
     } catch (error) {
         console.error(error);
     }
--- src/components/util/FetchWebsite.js
+++ src/components/util/FetchWebsite.js
@@ -3,7 +3,10 @@ import {
     doc,
     onSnapshot,
 } from "firebase/firestore";
-import React, { useState } from "react";
+import React, {
+    useEffect,
+    useState,
+} from "react";
 import { useDispatch } from "react-redux";
 import { db } from "../../firebase-setup";
 import { setWebsite } from "../../redux/appReducer";
@@ -12,19 +15,31 @@ export default function FetchWebsite() {
     const dispatch = useDispatch();
     // const docRef = doc(db, "website", section);
     try {
+        useEffect(() => {
+            const unsub = onSnapshot(
+                webRef,
+                (snapshot) => {
+                    const newData = [];
+                    snapshot.docs.forEach(
+                        (section) => {
+                            newData.push({
+                                ...section.data(),
+                                id: section.id,
+                            });
+                        }
+                    );
+                    // console.log(newData, dispatch);
+                    // setWebsite(newData);
+                    dispatch(setWebsite(newData));
+                }
+            );
+
+            return () => {
+                unsub();
+            };
+        }, []);
+
         const webRef = collection(db, "website");
-        onSnapshot(webRef, (snapshot) => {
-            const newData = [];
-            snapshot.docs.forEach((section) => {
-                newData.push({
-                    ...section.data(),
-                    id: section.id,
-                });
-            });
-            // console.log(newData, dispatch);
-            // setWebsite(newData);
-            dispatch(setWebsite(newData));
-        });
     } catch (error) {}
     return null;
 }
